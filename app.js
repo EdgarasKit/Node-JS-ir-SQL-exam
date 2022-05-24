1d76cff1ea6c313bdb6880ca36d158d91cef5853
EdgarasKit <edgar.kitkevich@gmail.com>
2022-05-24 15:21:54 +0300

    ui routes pajungimas

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import usersRouter from './routes/api/users.js';
 import blogaiRouter from './routes/api/blog.js';
+import blogaiUiRouter from './routes/ui/home.js';
+import registerRouter from './routes/ui/register.js';
+import loginRouter from './routes/ui/login.js';
+import userRouter from './routes/ui/user.js';
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -31,5 +35,9 @@ app.use(cookieParser());
 // Routes
 app.use('/api/users', usersRouter);
 app.use('/api/blog', blogaiRouter);
+app.use('/:home?', blogaiUiRouter);
+app.use('/register', registerRouter);
+app.use('/login', loginRouter);
+app.use('/user', userRouter);
 
 app.listen(PORT, ()=>console.log(`Serveris paleistas ant ${PORT} porto.`));
