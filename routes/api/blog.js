c87e8d4ee3138425a7565e37e696c849a36afd21
EdgarasKit <edgar.kitkevich@gmail.com>
2022-05-24 12:00:54 +0300

    blog isvedame pagal ID

--- routes/api/blog.js
+++ routes/api/blog.js
@@ -3,6 +3,7 @@ import con from '../../SQL_Connect.js';
 
 const router = express.Router();
 
+// isvedame viska i ekrana
 router.get('/', async (req, res) => {
     try {
         const [data] = await con.query("SELECT*FROM atsiskaitymas.blog");
@@ -13,6 +14,20 @@ router.get('/', async (req, res) => {
     }
 });
 
-
+// isvedame bloga pagal id
+router.get('/:id', async (req, res) => {
+    try {
+        const [data] = await con.query
+        (`SELECT * FROM atsiskaitymas.blog WHERE id=?`, [req.params.id]);
+        if (data.length === 0) {
+            res.send('Neteisingas ID, bandykite dar karta');
+        } else {
+            res.send(data);
+        }
+    } catch (err) {
+        console.log('Klaida');
+        res.send({ err: `Klaida: ${err}`});
+    }
+});
 
 export default router;
